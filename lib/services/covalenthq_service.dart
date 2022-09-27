c18414c85f5f103301d43ee0fc9711496eb5587c
Crypto Shadow <102347045+xcryptoshadow@users.noreply.github.com>
2022-09-27 13:33:23 +0530

    covalent api

---
+++ lib/services/covalenthq_service.dart
@@ -0,0 +1,41 @@
+import 'dart:io';
+
+import 'package:dio/dio.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+// TODO use interface to decouple dio package
+
+// COVALENT API
+final covalentDioProvider = Provider<Dio>((ref) {
+  const key = String.fromEnvironment('COVALENT_API_KEY');
+
+  return Dio(
+    BaseOptions(
+      baseUrl: "https://api.covalenthq.com/v1/",
+      queryParameters: {"key": key},
+    ),
+  )..interceptors.add(
+      LogInterceptor(
+        requestHeader: false,
+        // responseHeader: false,
+      ),
+    );
+});
+
+// Blockdaemon API
+final blockdaemonDioProvider = Provider<Dio>((ref) {
+  const key = String.fromEnvironment('BLOCKDAEMON_API_KEY');
+  return Dio(
+    BaseOptions(
+      baseUrl: "https://ubiquity.api.blockdaemon.com/v1/",
+      headers: {
+        HttpHeaders.authorizationHeader: key,
+      },
+    ),
+  )..interceptors.add(
+      LogInterceptor(
+        requestHeader: false,
+        // responseHeader: false,
+      ),
+    );
+});
